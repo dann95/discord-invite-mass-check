d8a0881ca033e64617556f931d7f424be58fcbd5
Daniel Rubin <danielrubin95@hotmail.com>
2018-04-11 09:52:28 -0300

    handle unhandled promise, and show done/total

--- check.js
+++ check.js
@@ -5,22 +5,42 @@ const _succ = process.argv[3]
 const _regex = /^https:\/\/discordapp.com\/invite/
 const discords = fs.readFileSync(_list).toString().split('\n').filter(d => _regex.test(d))
 
+'use strict';
+
 Array.prototype.forEachAsync = async function (fn) {
     for (let t of this) { await fn(t) }
 }
 
 let successDiscords = []
+let count = 0
 
 async function isValid(url) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url, {"waitUntil" : "networkidle0"})
-    const content = await page.evaluate(() => document.documentElement.innerHTML)
-    const i = content.indexOf('instant invite is invalid or has expired')
-    await browser.close();
-    if(i === -1) {
-      successDiscords.push(url)
+
+    process.on("unhandledRejection", (reason, p) => {
+      console.error("Unhandled Rejection at: Promise", p, "reason:", reason);
+      browser.close();
+    });
+
+    try {
+
+      await page.goto(url, {"waitUntil" : "networkidle0"})
+      const content = await page.evaluate(() => document.documentElement.innerHTML)
+      const i = content.indexOf('instant invite is invalid or has expired')
+      await browser.close();
+
+      if(i === -1) {
+        successDiscords.push(url)
+      }
+
+    }catch(error) {
+      console.log(error);
+      browser.close();
     }
+    count++
+    process.stdout.write('\x1Bc')
+    console.log(`${count}/${discords.length}`)
 }
 
 async function iterateDiscords() {
